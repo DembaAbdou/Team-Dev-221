c9199c54a07abc227ca0116a4d6d7c5a24bba090
DembaAbdou <senedemba98@gmail.com>
2022-02-16 20:13:11 +0100

    Liste des heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'DG Issa Sow' },
+  { id: 12, name: 'AD Le Véridique' },
+  { id: 13, name: 'Nullard M Bachir' },
+  { id: 14, name: 'Mbengue Sawaré' },
+  { id: 15, name: 'Gérard Laye ' },
+  { id: 16, name: 'Alliance Pendista' },
+  { id: 17, name: 'Zahra Fatim' },
+  { id: 18, name: 'Dr Khalil' },
+  { id: 19, name: 'SG Abdu' },
+  { id: 20, name: 'PCP Senhor Sen' }
+];
