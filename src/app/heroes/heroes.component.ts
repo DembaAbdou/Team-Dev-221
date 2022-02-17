aadca6397fb63720283eb8cc8116b2673321a5b2
DembaAbdou <senedemba98@gmail.com>
2022-02-17 12:47:07 +0100

    modified of services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 import { MessageService } from '../message.service';
-
-//import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
@@ -25,11 +23,11 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: SelectedHero id=${hero.id}');
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
-}
+  }
 }
--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MessageService {
-  message: string[] = [];
+  messages: string[] = [];
 
   add(message: string) {
-    this.message.push(message);
+    this.messages.push(message);
   }
 
   clear() {
-    this.message = [];
+    this.messages = [];
   }
-
-  constructor() { }
 }
